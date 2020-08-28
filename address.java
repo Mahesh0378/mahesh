f51d35e62dd943d7f8555d9202ef7cd2ed764c95
Mahesh0378 <70385247+Mahesh0378@users.noreply.github.com>
2020-08-28 20:46:35 +0530

    Add files via upload

---
+++ address.java
@@ -0,0 +1,16 @@
+package pack1;
+
+public class address {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+      String name ="mahesh";
+      String hno ="30-2-11/1";
+      String city ="kakinada";
+      System.out.println("name = "+name);
+      System.out.println("hno = "+hno);
+      System.out.println("city ="+city);
+      
+	}
+
+}
