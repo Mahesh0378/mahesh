8c1df68af3ba7262d76f7a5d52069a4a8fb54fee
Mahesh0378 <70385247+Mahesh0378@users.noreply.github.com>
2020-08-28 20:49:53 +0530

    Add files via upload

---
+++ Square.java
@@ -0,0 +1,15 @@
+package pack1;
+import java.util.*;
+public class Square {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+     Scanner sc = new Scanner(System.in);
+     System.out.println("enter side value:");
+     int side =sc.nextInt();
+     int  a = side*side;
+     System.out.println("area of square ="+a);
+     
+	}
+
+}
