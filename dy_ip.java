68c94cead68715a6b514409066a74972201300bf
Mahesh0378 <70385247+Mahesh0378@users.noreply.github.com>
2020-08-28 20:49:27 +0530

    Add files via upload

---
+++ dy_ip.java
@@ -0,0 +1,16 @@
+package pack1;
+import java.util.*;
+public class dy_ip {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub  
+      Scanner obj = new Scanner(System.in);
+      System.out.println("enter number1");
+      int a= obj.nextInt();
+      System.out.println("enter number2");
+      int b= obj.nextInt();
+      int c=a+b;
+      System.out.println("sum value ="+ c);
+	}
+
+}
